d38f384d7ff03e1e1f64697a6011038c8592797f
Andreas Heige <a.heige.83@gmail.com>
2020-06-24 23:10:28 +0200

    added template for blog

---
+++ GS/src/templates/blog-post.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+
+const BlogPost = () => {
+    return (
+        <Layout>
+            <div>This is the SPT</div>
+        </Layout>
+    )
+};
+
+export default BlogPost;
