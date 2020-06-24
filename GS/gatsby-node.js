cc1d3ddddb5739bb6c1cbbb119c3b562691d9ed7
Andreas Heige <a.heige.83@gmail.com>
2020-06-24 22:50:17 +0200

    generating slugs

--- GS/gatsby-node.js
+++ GS/gatsby-node.js
@@ -1,8 +1,13 @@
 const path = require('path');
 
-exports.onCreateNode = ({ node }) => {
+exports.onCreateNode = ({ node, actions }) => {
+    const { createNodeField } = actions
     if (node.internal.type === 'MarkdownRemark') {
         const slug = path.basename(node.fileAbsolutePath, '.md')
-        console.log(slug)
+        createNodeField({
+            node,
+            name: 'slug',
+            value: slug,
+        })
     }
 };
